a1c529f471d90410778303e6a055f942e8e4a311
Peter Hoyes <pah58@pip.srcf.societies.cam.ac.uk>
2012-05-23 05:04:47 +0100

    Ignoring timezone requirement

--- app/check.php
+++ app/check.php
@@ -24,7 +24,7 @@ echo "** WARNING **\n";
 // mandatory
 echo_title("Mandatory requirements");
 check(version_compare(phpversion(), '5.3.2', '>='), sprintf('Checking that PHP version is at least 5.3.2 (%s installed)', phpversion()), 'Install PHP 5.3.2 or newer (current version is '.phpversion(), true);
-check(ini_get('date.timezone'), 'Checking that the "date.timezone" setting is set', 'Set the "date.timezone" setting in php.ini (like Europe/Paris)', true);
+//check(ini_get('date.timezone'), 'Checking that the "date.timezone" setting is set', 'Set the "date.timezone" setting in php.ini (like Europe/Paris)', true);
 check(is_writable(__DIR__.'/../app/cache'), sprintf('Checking that app/cache/ directory is writable'), 'Change the permissions of the app/cache/ directory so that the web server can write in it', true);
 check(is_writable(__DIR__.'/../app/logs'), sprintf('Checking that the app/logs/ directory is writable'), 'Change the permissions of the app/logs/ directory so that the web server can write in it', true);
 check(function_exists('json_encode'), 'Checking that the json_encode() is available', 'Install and enable the json extension', true);
