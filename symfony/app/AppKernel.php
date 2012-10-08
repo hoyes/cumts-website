d62e7246b3c934cda4e404a5a9a5fed4216785ac
Peter Hoyes <pah58@pip.srcf.societies.cam.ac.uk>
2012-10-08 23:16:03 +0100

    Added Raven auth bundle

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Cumts\MainBundle\CumtsMainBundle(),
             new Cumts\AdminBundle\CumtsAdminBundle(),
+	    new Misd\RavenBundle\MisdRavenBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
