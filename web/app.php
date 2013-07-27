83e5cdb9ac8c090b9abdddcb97f4893388c17f8a
Peter Hoyes <pah58@pip.srcf.societies.cam.ac.uk>
2013-07-27 16:24:10 +0100

    Upgraded to Symfony 2.3

--- web/app.php
+++ web/app.php
@@ -4,7 +4,7 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-
+error_reporting(E_ERROR | E_WARNING);
 // Use APC for autoloading to improve performance
 // Change 'sf2' by the prefix you want in order to prevent key conflict with another application
 /*
